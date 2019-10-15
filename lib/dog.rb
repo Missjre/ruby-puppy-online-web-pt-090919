4cd905b3687e03a7a2c5791af60df4fa74878fa3
Jalysa Reason <jalysareason@gmail.com>
2019-10-15 01:51:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,12 @@
-# Add your code here
+class Dog
+ 
+  @@all = []
+ 
+  attr_accessor :name :breed :owner
+ 
+  def initialize(name)
+    self.name = name
+    self.breed = breed
+    self.owner = owner
+    
+end
